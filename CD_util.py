385667454b566adec9835ab5fbfef896aca159c0
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2021-12-06 15:35:28 +0330

    feat(CD):add graph genration method and timeout decorator

--- CD_util.py
+++ CD_util.py
@@ -6,6 +6,47 @@ from cdlib import algorithms, evaluation
 import networkx as nx
 
 from sklearn.metrics import normalized_mutual_info_score
+from networkx.generators.community import LFR_benchmark_graph as lfr
+
+
+### utils
+
+import sys
+import threading
+from time import sleep
+
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+
+
+def quit_function(fn_name):
+    # print to stderr, unbuffered in Python 2.
+    print("{0} took too long".format(fn_name), file=sys.stderr)
+    sys.stderr.flush()  # Python 3 stderr is likely buffered.
+    thread.interrupt_main()  # raises KeyboardInterrupt
+
+
+def exit_after(s):
+    """
+    use as decorator to exit process if
+    function takes longer than s seconds
+    """
+
+    def outer(fn):
+        def inner(*args, **kwargs):
+            timer = threading.Timer(s, quit_function, args=[fn.__name__])
+            timer.start()
+            try:
+                result = fn(*args, **kwargs)
+            finally:
+                timer.cancel()
+            return result
+
+        return inner
+
+    return outer
 
 
 def draw_graph_com(G, coms, fig_size=(8, 8)):
@@ -73,7 +114,70 @@ def to_vertex_community(coms):
     ]
 
 
-def normalized_mutual_info_score(coms_pred, coms_true):
+def normalized_mutual_info_acc(coms_pred, coms_true):
     return normalized_mutual_info_score(
         to_vertex_community(coms_true), to_vertex_community(coms_pred)
     )
+
+
+### generate graph ###
+
+
+@exit_after(5)
+def generate_graph(n, tau1, tau2, mu, average_degree, min_community):
+    return lfr(
+        n, tau1, tau2, mu, average_degree=average_degree, min_community=min_community
+    )
+
+
+def generate_random_lfr(
+    n_inter,
+    tau1_inter,
+    tau2_inter,
+    mu_inter,
+    average_degree_inter,
+    min_community_inter,
+    max_iter=10,
+):
+    generated = False
+    max_iter = 10
+    count = 1
+    G = None
+    while not generated or count < max_iter:
+        count += 1
+        try:
+            n = np.random.randint(n_inter[0], n_inter[1])
+            tau1 = np.random.uniform(tau1_inter[0], tau1_inter[1])
+            tau2 = np.random.uniform(tau2_inter[0], tau2_inter[1])
+            mu = np.random.uniform(mu_inter[0], mu_inter[1])
+            average_degree = np.random.randint(
+                average_degree_inter[0], average_degree_inter[1]
+            )
+            min_community = np.random.randint(
+                min_community_inter[0], min_community_inter[1]
+            )
+            G = generate_graph(82, 3, 1.3, 0.18, average_degree=5, min_community=50)
+            generated = True
+        except:
+            generated = False
+            continue
+    if not generated:
+        raise ("Max iter exceeded")
+
+    return G
+
+
+class Parameter:
+    def __init__(self, name: str, value) -> None:
+        self.name = name
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"{self.name} : {str(self.value)}"
+
+
+class Experiment:
+    def __init__(self, parameter_initalizer) -> None:
+        self.parameters = []
+        self.parameters = parameter_initalizer(self.parameters)
+        self.metrics = []
